138298bc20c3b7467c9a10f8620095ae2c10855a
Samuel Miksits <samuel.miksits@gmail.com>
2025-10-01 11:02:14 +0200

    examples: Add process type definitions to SDF_example_001

--- examples/model/SDF_example_001.hs
+++ examples/model/SDF_example_001.hs
@@ -14,14 +14,18 @@ system s_in = (s_out1, s_out2) where
     -- Process D
     s_out2 = processD s_b_d s_c_d
 
+processA :: Signal Int -> (Signal Int, Signal Int)
 processA s_in = actor12SDF 2 (1,1) f s_in where
     f [a, b] = ([2*a], [3*b])
 
+processB :: Signal Int -> (Signal Int, Signal Int)
 processB s_in = actor12SDF 1 (1, 1) f s_in where
     f [a] = ([a-2], [a-1])
 
+processC :: Signal Int -> Signal Int
 processC s_in = actor11SDF 1 1 f s_in where
     f [a] = [a+1]
 
+processD :: Signal Int -> Signal Int -> Signal Int
 processD s_in1 s_in2 = actor21SDF (1, 1) 1 f s_in1 s_in2 where
     f [a] [b] = [a*b]
