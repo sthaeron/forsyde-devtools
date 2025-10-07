f3f0dbb6e6e87fae8adc260901d4cb65f1ff7b2a
Samuel Miksits <samuel.miksits@gmail.com>
2025-10-07 22:12:19 +0200

    feat: Argument parsing for Haskell

---
+++ src/Arguments.hs
@@ -0,0 +1,171 @@
+module Arguments where
+
+import Options.Applicative
+import System.IO
+
+-- Which file to use for input
+data Input
+  = InputFile FilePath
+
+-- Output, can either be a file or stdout
+data Output
+  = OutputFile (FilePath, HasExtension)
+  | StdOut
+
+data HasExtension
+  = WithFileExtension
+  | NoFileExtension
+
+-- Stubbed out this type for now, will probably not implement
+-- reading separate input formats
+data InputFormat
+  = InputForSyDe
+  | InputCore
+  | InputIRForSyDe
+  | InputIRProcedural
+
+data OutputFormat
+  = OutputC
+  | OutputCore
+  | OutputIRForSyDe
+  | OutputIRProcedural
+
+data Arguments = Arguments
+  { -- Files
+    input :: Input,
+    output :: Output,
+    -- Formats
+    output_format :: OutputFormat
+  }
+
+-- Handle file input, always need to define a file
+inputFile :: Parser Input
+inputFile =
+  InputFile
+    <$> strArgument
+      ( metavar "INPUT"
+          <> help "Input filename"
+      )
+
+outputFileTop :: Parser Output
+outputFileTop =
+  outputFile
+    <|> outputFileStdOut
+
+-- Function to create a filepath with file extension
+fileName :: ReadM (FilePath, HasExtension)
+fileName = str >>= returnFile
+  where
+    returnFile s = pure (s, WithFileExtension)
+
+-- Output to file optionally, otherwise stdout by default
+outputFile :: Parser Output
+outputFile =
+  option
+    (OutputFile <$> fileName)
+    ( short 'o'
+        <> long "output"
+        <> metavar "OUTPUT"
+        <> value (OutputFile ("main", NoFileExtension))
+        <> help "Output filename"
+    )
+
+outputFileStdOut :: Parser Output
+outputFileStdOut =
+  flag'
+    StdOut
+    ( long "stdout"
+        <> help "Print output to stdout"
+    )
+
+-- Top-level. All of these 4 flags are optional, but one of them
+-- need to be active. How does that work? inputFormatCore has a
+-- default value for InputForSyde if no flag is provided
+inputFormatTop :: Parser InputFormat
+inputFormatTop =
+  inputFormatCore
+    <|> inputFormatForSyDeIR
+    <|> inputFormatProceduralIR
+
+inputFormatCore :: Parser InputFormat
+inputFormatCore =
+  flag
+    InputForSyDe
+    InputCore
+    ( long "input-core"
+        <> help "Input file in ForSyDe"
+    )
+
+inputFormatForSyDeIR :: Parser InputFormat
+inputFormatForSyDeIR =
+  flag'
+    InputIRForSyDe
+    ( long "input-ir-forsyde"
+        <> help "Input file in ForSyDe-IR"
+    )
+
+inputFormatProceduralIR :: Parser InputFormat
+inputFormatProceduralIR =
+  flag'
+    InputIRProcedural
+    ( long "input-ir-procedural"
+        <> help "Input file in Procedural-IR"
+    )
+
+-- Top-level. All of these 4 flags are optional, but one of them
+-- need to be active. Same as inputFormatTop, where the function
+-- for FormatCore has InputC as default but InputCore if flag is
+-- turned on
+outputFormatTop :: Parser OutputFormat
+outputFormatTop =
+  outputFormatC
+    <|> outputFormatForSyDeIR
+    <|> outputFormatProceduralIR
+
+-- Temporary implementation. Sets output to core as default functionality,
+outputFormatC :: Parser OutputFormat
+outputFormatC =
+  flag
+    OutputCore
+    OutputC
+    ( long "output-c"
+        <> help "Output file in C"
+    )
+
+-- Temporarily commented out, this is how it will work in the end where the
+-- Core output format function has C as default if flag is unspecified, and Core
+-- if set.
+{-
+outputFormatCore :: Parser OutputFormat
+outputFormatCore =
+  flag
+    OutputC
+    OutputCore
+    ( long "output-core"
+        <> help "Output file in Core"
+    )
+-}
+
+outputFormatForSyDeIR :: Parser OutputFormat
+outputFormatForSyDeIR =
+  flag'
+    OutputIRForSyDe
+    ( long "output-ir-forsyde"
+        <> help "Output file in ForSyDe-IR"
+    )
+
+outputFormatProceduralIR :: Parser OutputFormat
+outputFormatProceduralIR =
+  flag'
+    OutputIRProcedural
+    ( long "output-ir-procedural"
+        <> help "Output file in Procedural-IR"
+    )
+
+-- Top level argument parsing function, takes 4 flags.
+arguments :: Parser Arguments
+arguments =
+  Arguments
+    <$> inputFile
+    <*> outputFileTop
+    <*> outputFormatTop
--- src/Main.hs
+++ src/Main.hs
@@ -1,37 +1,116 @@
 module Main where
 
+import Arguments
 import GHC
-import GHC.Driver.Ppr
-import GHC.Paths
-import GHC.Utils.Outputable
+import GHC.Data.EnumSet (fromList)
 import GHC.Driver.Monad
-import GHC.Data.EnumSet ( fromList )
+import GHC.Driver.Ppr
+import GHC.Driver.Session (defaultFatalMessager, defaultFlushOut)
+import GHC.Paths
+import GHC.Utils.Outputable hiding ((<>))
+import Options.Applicative
 
-import GHC.Driver.Session ( defaultFatalMessager, defaultFlushOut )
+{-
 
-main = 
-    defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
-      coreIR <- runGhc (Just libdir) $ do
-        dflags <- getSessionDynFlags
-        setSessionDynFlags dflags { 
-            ghcLink = LinkInMemory,
+    Specification
+
+    Compiler steps:
+
+    ForSyDe model ->
+    Core ->
+    ForSyDe IR ->
+    Procedural IR ->
+    C
+
+    - First we need a source file.
+    - Input mode:
+        - This should be the format for the input file, if not otherwise
+          specified, this should be a ForSyDe model (.hs) file
+
+    - Output mode:
+        - This should be the format of the output file, if not otherwise
+          specified, this should be C code
+        - if --output-core, output should be in core format
+        - if --output-ir-forsyde, output should be in ForSyDe-IR format
+        - if --output-ir-procedural, output should be in procedural IR format
+
+    - Output file argument:
+        - Use "-o" flag, outputs to desired file
+        - Dump to stdout if unspecified
+
+-}
+
+toCore :: FilePath -> IO CoreModule
+toCore input_file =
+  defaultErrorHandler defaultFatalMessager defaultFlushOut $ do
+    runGhc (Just libdir) $ do
+      dflags <- getSessionDynFlags
+      setSessionDynFlags
+        dflags
+          { ghcLink = LinkInMemory,
             ghcMode = CompManager,
-            generalFlags = fromList [
-                Opt_SuppressTicks,
-                Opt_SuppressCoercions,
-                Opt_SuppressCoercionTypes,
-                Opt_SuppressVarKinds,
-                Opt_SuppressModulePrefixes,
-                Opt_SuppressTypeApplications,
-                Opt_SuppressIdInfo,
-                Opt_SuppressUnfoldings,
-                Opt_SuppressTypeSignatures,
-                Opt_SuppressUniques,
-                Opt_SuppressStgExts,
-                Opt_SuppressStgReps,
-                Opt_SuppressTimestamps,
-                Opt_SuppressCoreSizes
-            ]
-        }
-        compileToCoreModule "examples/model/SDF_example_002.hs"
-      putStrLn $ showSDocUnsafe $ ppr coreIR
+            generalFlags =
+              fromList
+                [ Opt_SuppressTicks,
+                  Opt_SuppressCoercions,
+                  Opt_SuppressCoercionTypes,
+                  Opt_SuppressVarKinds,
+                  Opt_SuppressModulePrefixes,
+                  Opt_SuppressTypeApplications,
+                  Opt_SuppressIdInfo,
+                  Opt_SuppressUnfoldings,
+                  Opt_SuppressTypeSignatures,
+                  Opt_SuppressUniques,
+                  Opt_SuppressStgExts,
+                  Opt_SuppressStgReps,
+                  Opt_SuppressTimestamps,
+                  Opt_SuppressCoreSizes
+                ]
+          }
+      compileToCoreModule input_file
+
+write_output :: Output -> OutputFormat -> [Char] -> IO ()
+write_output StdOut _ s =
+  putStrLn s
+write_output (OutputFile (output_file, WithFileExtension)) _ s =
+  writeFile output_file s
+write_output (OutputFile (output_file, NoFileExtension)) extension s =
+  writeFile (generateDefaultFileName output_file extension) s
+
+-- Function for generating default file names
+generateDefaultFileName :: FilePath -> OutputFormat -> FilePath
+generateDefaultFileName f OutputC = f ++ ".c"
+generateDefaultFileName f OutputCore = f ++ ".hcr"
+generateDefaultFileName f OutputIRForSyDe = f ++ ".irf"
+generateDefaultFileName f OutputIRProcedural = f ++ ".irp"
+
+-- Main function for running after arguments have been returned from main.
+-- Need to pattern match based on the arguments used. They are matched on
+-- manually since a lot of combinations are invalid.
+-- You can't for intance start with procedural IR as input and go to Core
+-- since the compiler doesn't go backwards.
+
+run :: Arguments -> IO ()
+-- "Normal run"
+run (Arguments (InputFile input_file) output_file OutputC) =
+  putStrLn "To C"
+run (Arguments (InputFile input_file) output_file OutputIRForSyDe) =
+  putStrLn "To ForSyDe IR"
+run (Arguments (InputFile input_file) output_file OutputIRProcedural) =
+  putStrLn "To Procedural IR"
+-- What we have so far, take input file and write out core
+run (Arguments (InputFile input_file) outputFile OutputCore) = do
+  core_output <- toCore input_file
+  dflags <- runGhc (Just libdir) $ getSessionDynFlags
+  write_output outputFile OutputCore (showSDoc dflags $ ppr core_output)
+
+main :: IO ()
+main = run =<< execParser opts
+  where
+    opts =
+      info
+        (arguments <**> helper)
+        ( fullDesc
+            <> progDesc "Compile a ForSyDe model"
+            <> header "ForSyDe DevTools"
+        )
