a863e66bd77e9c3fbc6c305fa9dd046cb1508932
Michel <mdelliabo@gmail.com>
2025-10-14 15:54:48 +0200

    refined Core IR pretty print

--- src/CoreIR.hs
+++ src/CoreIR.hs
@@ -26,13 +26,13 @@ import Text.Printf (printf)
 prettyCoreBind :: CoreBind -> String
 prettyCoreBind bind = case bind of
   NonRec b e -> printf "NonRec(%s: \n\t%s)" (prettyVar b) (prettyCoreExpr e)
-  Rec bindExprList -> printf "Rec(%s)" (prettyBindExprList bindExprList)
+  Rec bindExprList -> printf "Rec({%s})" (prettyBindExprList bindExprList)
 
 prettyCoreBindList :: [CoreBind] -> String
 prettyCoreBindList binds = concatMap (\b -> prettyCoreBind b ++ "\n\n") binds
 
 prettyBindExpr :: (Var, CoreExpr) -> String
-prettyBindExpr (b, e) = printf "(%s: \n\t%s), " (prettyVar b) (prettyCoreExpr e)
+prettyBindExpr (b, e) = printf "(%s, %s), " (prettyVar b) (prettyCoreExpr e)
 
 prettyBindExprList :: [(Var, CoreExpr)] -> String
 prettyBindExprList bexprs = concatMap prettyBindExpr bexprs
@@ -41,11 +41,11 @@ prettyCoreExpr :: CoreExpr -> String
 prettyCoreExpr expr = case expr of
   Var i -> printf "Var(%s)" (showSDocUnsafe (ppr i))
   Lit l -> printf "Lit(%s)" (showSDocUnsafe (ppr l))
-  App e a -> printf "App(%s: \n\tArg(%s))" (prettyCoreExpr e) (prettyCoreExpr a)
-  Lam b e -> printf "Lam(%s: \n\tExpr(%s))" (prettyVar b) (prettyCoreExpr e)
+  App e a -> printf "App(%s * \n\t%s)" (prettyCoreExpr e) (prettyCoreExpr a)
+  Lam b e -> printf "Lam(%s -> \n\t%s)" (prettyVar b) (prettyCoreExpr e)
   Type t -> printf "Type(%s)" (showSDocUnsafe (ppr t))
-  Let bind e -> printf "Let(\n\tBind(%s)\n\t%s)" (prettyCoreBind bind) (prettyCoreExpr e)
-  Case e b t alts -> printf "Case(%s: \n\t(%s, %s){%s})" (prettyCoreExpr e) (prettyVar b) (showSDocUnsafe (ppr t)) (prettyCoreAltList alts)
+  Let bind e -> printf "Let(%s =\n\t %s)" (prettyCoreBind bind) (prettyCoreExpr e)
+  Case e b t alts -> printf "Case(%s: (%s, %s) \n\t{%s})" (prettyCoreExpr e) (prettyVar b) (showSDocUnsafe (ppr t)) (prettyCoreAltList alts)
   Cast e co -> printf "Cast(%s: \n\tCoercion(%s))" (prettyCoreExpr e) (showSDocUnsafe (ppr co))
   Tick t e -> printf "Tick(%s: \n\t%s)" (showSDocUnsafe (ppr t)) (prettyCoreExpr e)
   Coercion co -> printf "Coercion(%s)" (showSDocUnsafe (ppr co))
@@ -57,16 +57,16 @@ prettyVar :: Var -> String
 prettyVar v = showSDocUnsafe (ppr v)
 
 prettyCoreBndrList :: [Var] -> String
-prettyCoreBndrList bndrs = concatMap (\x -> printf "(%s), " (prettyVar x)) bndrs
+prettyCoreBndrList bndrs = concatMap (\x -> printf "%s, " (prettyVar x)) bndrs
 
 prettyCoreAltCon :: AltCon -> String
 prettyCoreAltCon altCons = case altCons of
   DataAlt d -> printf "DataAlt(%s)" (showSDocUnsafe (ppr d))
   LitAlt l -> printf "LitAlt(%s)" (showSDocUnsafe (ppr l))
-  DEFAULT -> "DEFAULT()"
+  DEFAULT -> "DEFAULT"
 
 prettyCoreAltList :: [CoreAlt] -> String
-prettyCoreAltList alts = concatMap (\x -> printf "(%s), " (prettyCoreAlt x)) alts
+prettyCoreAltList alts = concatMap (\x -> printf "%s, " (prettyCoreAlt x)) alts
 
 compileToDesugar :: FilePath -> IO CoreProgram
 compileToDesugar filePath = runGhc (Just libdir) $ do
