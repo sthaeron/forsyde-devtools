a65685730108a8b17c7eea67a810791693eade8c
Klara Modin <klarasmodin@gmail.com>
2025-11-02 17:16:22 +0100

    send the diagram/accept messages as expected by KLighD

--- src/LSP.hs
+++ src/LSP.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main (main) where
@@ -11,21 +12,134 @@ import Control.Concurrent (forkFinally)
 import qualified Control.Exception as E
 import Control.Monad (forever, void)
 import Control.Monad.IO.Class
+import Data.Aeson ((.=))
+import qualified Data.Aeson as A
 import qualified Data.List.NonEmpty as NE
 import Data.Proxy
+import qualified Data.Sequence as Seq
 import qualified Data.Text as T
 import Language.LSP.Protocol.Message
 import Language.LSP.Server
 import Network.Socket
 import Prettyprinter
+import SKGraphSchema
 import System.IO
+
+diagramAcceptMethod :: SMethod (Method_CustomMethod "diagram/accept")
+diagramAcceptMethod = (SMethod_CustomMethod (Proxy @"diagram/accept"))
+
+setPreferencesMethod :: SMethod (Method_CustomMethod "keith/preferences/setPreferences")
+setPreferencesMethod = (SMethod_CustomMethod (Proxy @"keith/preferences/setPreferences"))
+
+setSynthesis :: A.Value
+setSynthesis =
+  A.object
+    [ "clientId" .= T.pack "sprotty",
+      "action"
+        .= A.object
+          [ "kind" .= T.pack "setSyntheses",
+            "syntheses"
+              .= Seq.fromList
+                [ A.object
+                    [ "id" .= T.pack "se.kth.forsyde-devtools.dummyid",
+                      "displayName" .= T.pack "ForSyDe Shallow"
+                    ]
+                ]
+          ]
+    ]
+
+updateOptions :: A.Value
+updateOptions =
+  A.object
+    [ "clientId" .= T.pack "sprotty",
+      "action"
+        .= A.object
+          [ "kind" .= T.pack "updateOptions",
+            "valuedSynthesisOptions" .= (Seq.empty :: Seq.Seq A.Object),
+            "layoutOptions" .= (Seq.empty :: Seq.Seq A.Object),
+            "actions" .= (Seq.empty :: Seq.Seq A.Object),
+            "modelUri" .= T.pack "file:///home/klara/git/plyghd-ls-demonstrator/empty.kgt"
+          ]
+    ]
+
+requestBounds :: A.Value
+requestBounds =
+  A.object
+    [ "clientId" .= T.pack "sprotty",
+      "action"
+        .= A.object
+          [ "kind" .= T.pack "requestBounds",
+            "newRoot"
+              .= KGraph
+                { gid = T.pack "file:///home/klara/git/plyghd-ls-demonstrator/empty.kgt",
+                  properties = [],
+                  child =
+                    KNode
+                      { gid = "$root",
+                        renderings = [],
+                        properties = [],
+                        children =
+                          [ KNode
+                              { children =
+                                  [ KLabel {label = "A", gid = "$root$Na$$L0"},
+                                    KPort
+                                      { children = [],
+                                        renderings = [],
+                                        properties = [],
+                                        gid = "$root$Na$$P0"
+                                      }
+                                  ],
+                                renderings = [KEllipse],
+                                properties =
+                                  [ (NodeLabelsPlacement, [1, 4, 6]),
+                                    (NodeSizeConstraints, [3]),
+                                    (NodeSizeMinimum, [64, 64])
+                                  ],
+                                gid = "$root$Na"
+                              },
+                            KNode
+                              { children =
+                                  [ KLabel {label = "B", gid = "$root$Nb$$L0"},
+                                    KPort
+                                      { children = [],
+                                        renderings = [],
+                                        properties = [],
+                                        gid = "$root$Nb$$P0"
+                                      }
+                                  ],
+                                renderings = [KEllipse],
+                                properties =
+                                  [ (NodeLabelsPlacement, [1, 4, 6]),
+                                    (NodeSizeConstraints, [3]),
+                                    (NodeSizeMinimum, [64, 64])
+                                  ],
+                                gid = "$root$Nb"
+                              },
+                            KEdge
+                              { children = [],
+                                renderings = [KPolyline],
+                                properties = [],
+                                gid = "$root$Na$$P0$E0",
+                                source = "$root$Na$$P0",
+                                target = "$root$Nb$$P0"
+                              }
+                          ]
+                      }
+                }
+          ]
+    ]
 
 handlers :: Handlers (LspM ())
 handlers =
   mconcat
     [ notificationHandler SMethod_Initialized $ \_not -> do
         pure (),
-      requestHandler (SMethod_CustomMethod (Proxy @"diagram/accept")) $ \_req _resp -> do
+      notificationHandler setPreferencesMethod $ \_not -> do
+        pure (),
+      notificationHandler diagramAcceptMethod $ \_not -> do
+        sendNotification diagramAcceptMethod setSynthesis
+        sendNotification diagramAcceptMethod updateOptions
+        sendNotification diagramAcceptMethod requestBounds
         pure ()
     ]
 
