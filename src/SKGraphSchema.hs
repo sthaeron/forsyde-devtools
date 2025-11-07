b428bde338564f4a77c32ed58c908a7302b8e3cf
Klara Modin <klarasmodin@gmail.com>
2025-11-07 17:43:43 +0100

    add basic outline of SKGraphSchema

---
+++ src/SKGraphSchema.hs
@@ -0,0 +1,163 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+
+module SKGraphSchema where
+
+import Data.Aeson ((.=))
+import qualified Data.Aeson as A
+import qualified Data.Map as M
+import qualified Data.Sequence as Seq
+import qualified Data.Text as T
+
+data GraphElement
+  = KLabel
+      { label :: !T.Text,
+        gid :: !T.Text
+      }
+  | KNode
+      { children :: ![GraphElement],
+        renderings :: ![KRendering],
+        properties :: !KProperties,
+        gid :: !T.Text
+      }
+  | KPort
+      { children :: ![GraphElement],
+        renderings :: ![KRendering],
+        properties :: !KProperties,
+        gid :: !T.Text
+      }
+  | KEdge
+      { children :: ![GraphElement],
+        renderings :: ![KRendering],
+        properties :: !KProperties,
+        gid :: !T.Text,
+        source :: !T.Text,
+        target :: !T.Text
+      }
+  | KGraph
+      { child :: !GraphElement,
+        properties :: !KProperties,
+        gid :: !T.Text
+      }
+
+data KProperty
+  = NodeLabelsPlacement
+  | NodeSizeConstraints
+  | NodeSizeMinimum
+  | EdgeType
+  | JunctionPoints
+
+instance Show KProperty where
+  show NodeLabelsPlacement = "org.eclipse.elk.nodeLabels.placement"
+  show NodeSizeConstraints = "org.eclipse.elk.nodeSize.constraints"
+  show NodeSizeMinimum = "org.eclipse.elk.nodeSize.minimum"
+  show EdgeType = "org.eclipse.elk.edge.type"
+  show JunctionPoints = "org.eclipse.elk.junctionPoints"
+
+type KProperties = [(KProperty, [Int])]
+
+data KRendering
+  = KEllipse
+  | KPolyline
+  | KRectangle
+
+instance A.ToJSON KRendering where
+  toJSON rendering = case rendering of
+    KEllipse -> simple "KEllipseImpl"
+    KPolyline -> simple "KPolylineImpl"
+    KRectangle -> simple "KRectangleImpl"
+    where
+      simple r =
+        A.object
+          [ "type" .= (T.pack r),
+            "children" .= (Seq.empty :: Seq.Seq A.Object),
+            "actions" .= (Seq.empty :: Seq.Seq A.Object),
+            "styles" .= (Seq.empty :: Seq.Seq A.Object),
+            "properties"
+              .= A.object
+                [ "klighd.lsp.rendering.id" .= T.pack "$R0"
+                ]
+          ]
+
+instance A.ToJSON GraphElement where
+  toJSON element = case element of
+    KNode
+      { children = c,
+        renderings = r,
+        properties = p,
+        gid = i
+      } -> simple "node" c r p i
+    KLabel {label = l, gid = i} ->
+      A.object
+        [ "type" .= T.pack "label",
+          "text" .= l,
+          "id" .= i,
+          "properties"
+            .= A.object
+              [],
+          "data"
+            .= Seq.fromList
+              [ A.object
+                  [ "actions" .= (Seq.empty :: Seq.Seq A.Object),
+                    "children" .= (Seq.empty :: Seq.Seq A.Object),
+                    "properties"
+                      .= A.object
+                        [ "klighd.lsp.rendering.id" .= T.pack "$R0"
+                        ],
+                    "styles" .= (Seq.empty :: Seq.Seq A.Object),
+                    "text" .= l,
+                    "type" .= T.pack "KTextImpl"
+                  ]
+              ],
+          "children" .= (Seq.empty :: Seq.Seq A.Object)
+        ]
+    KPort
+      { children = c,
+        renderings = r,
+        properties = p,
+        gid = i
+      } -> simple "port" c r p i
+    KEdge
+      { children = c,
+        renderings = r,
+        properties = p,
+        gid = i,
+        source = s,
+        target = t
+      } ->
+        A.object
+          [ "data" .= Seq.fromList r,
+            "type" .= T.pack "edge",
+            "id" .= i,
+            "properties" .= (toProperties p),
+            "children"
+              .= Seq.fromList c,
+            "sourceId" .= s,
+            "targetId" .= t,
+            "junctionPoints" .= A.object []
+          ]
+    KGraph
+      { child = c,
+        properties = p,
+        gid = i
+      } ->
+        A.object
+          [ "type" .= T.pack "graph",
+            "revision" .= (0 :: Int),
+            "id" .= i,
+            "properties" .= (toProperties p),
+            "children"
+              .= Seq.fromList [c]
+          ]
+    where
+      simple t c r p i =
+        A.object
+          [ "data" .= Seq.fromList r,
+            "type" .= T.pack t,
+            "id" .= i,
+            "properties" .= (toProperties p),
+            "children"
+              .= Seq.fromList c
+          ]
+      toProperties p = M.fromList $ map (\(a, b) -> (T.pack $ show a, Seq.fromList b)) p
