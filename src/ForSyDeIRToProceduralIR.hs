e073ac73f347588f36776e8feecc7e05888d1b27
sthaeron <sebastian.thaeron@gmail.com>
2025-11-21 16:59:24 +0100

    reafactor: adds comments to current ForSyDe IR to Procedural IR translation

--- src/ForSyDeIRToProceduralIR.hs
+++ src/ForSyDeIRToProceduralIR.hs
@@ -8,54 +8,65 @@ import GHC.Types.Literal
 import ProceduralIR
 import Prelude hiding (id, init)
 
+-- | The `TranslationContext` is a data type which is used to pass around
+-- context required to complete the translation of ForSyDeIR to
+-- ProceduralIR.
 data TranslationContext = TranslationContext
   { flags :: DynFlags, -- Stores `DynFlags` for safely obtaining strings
-    ir_signals :: [(String, IRSignal)],
-    actors :: [(String, [Statement])],
-    signals :: [(String, Statement)],
-    io_temporary_tokens :: [Statement],
-    init_buffers :: [Statement],
-    free_buffers :: [Statement],
-    inputs :: [String],
-    outputs :: [String],
-    delayBuffers :: [(String, String)],
-    delays :: [Statement]
+    lookupSignals :: [(String, IRSignal)],
+    actors :: [(String, [Statement])], -- Associated list of actor ids and statements
+    signals :: [(String, Statement)], -- Associated list of signal ids and statement
+    ioTokens :: [Statement], -- List of statements for input/output tokens
+    initBuffers :: [Statement], -- List of statements for initialising signal buffers
+    freeBuffers :: [Statement], -- List of statements for freeing signal buffers
+    systemInputs :: [String], -- List of system inputs
+    systemOutputs :: [String], -- List of system outputs
+    delayBuffers :: [(String, String)], -- Associated list of signal ids and buffer name for delay signals
+    initDelay :: [Statement] -- List of statements for initialising delay tokens
   }
 
 initialTranslationContext :: DynFlags -> [(String, IRSignal)] -> [String] -> [String] -> [(String, String)] -> TranslationContext
-initialTranslationContext dflags lookupSignals inputList outputList delayBufferList =
+initialTranslationContext dflags lookupSignalList inputList outputList delayBufferList =
   TranslationContext
     { flags = dflags,
-      ir_signals = lookupSignals,
+      lookupSignals = lookupSignalList,
       actors = [],
       signals = [],
-      io_temporary_tokens = [],
-      init_buffers = [],
-      free_buffers = [],
-      inputs = inputList,
-      outputs = outputList,
+      ioTokens = [],
+      initBuffers = [],
+      freeBuffers = [],
+      systemInputs = inputList,
+      systemOutputs = outputList,
       delayBuffers = delayBufferList,
-      delays = []
+      initDelay = []
     }
 
+-- | Translates a ForSyDe IR `IRSystem` into a Procedural IR `Program`. Requires
+-- the schedule, buffer, and delay buffer lists from the SDF scheduler as an
+-- input. Also requires an associated list of signals for lookups.
 translateIRSystemToProgram :: DynFlags -> [String] -> [(String, Int)] -> [(String, String)] -> [(String, IRSignal)] -> IRSystem -> Program
-translateIRSystemToProgram dflags scheduleList bufferList delayBufferList lookupSignals (IRSystem (systemInputs, systemOutputs) constructors signals functions) =
-  let initialContext = initialTranslationContext dflags lookupSignals systemInputs systemOutputs delayBufferList
+translateIRSystemToProgram dflags scheduleList bufferList delayBufferList lookupSignalList (IRSystem (inputList, outputList) constructors _signalList functions) =
+  let initialContext = initialTranslationContext dflags lookupSignalList inputList outputList delayBufferList
       context1 = foldl translateIRConstructor initialContext constructors
       context2 = foldl translateBuffer context1 bufferList
       globals = concatMap (translateIRFunctionToGlobals context2) functions
       main = translateContextToMain context2 scheduleList
    in Prog (globals ++ [main])
 
+-- | Translates the `TranslationContext` and Schedule into a main function.
 translateContextToMain :: TranslationContext -> [String] -> Global
 translateContextToMain context scheduleList =
   let scheduledStmts = scheduleActors context scheduleList
       whileStmt = SWhile (EInt 1) (SScope scheduledStmts)
-      mainInitStmts = init_buffers context ++ io_temporary_tokens context ++ delays context
-      mainFreeStmts = free_buffers context ++ [SReturn (Just (EInt 0))]
+      mainInitStmts = initBuffers context ++ ioTokens context ++ initDelay context
+      mainFreeStmts = freeBuffers context ++ [SReturn (Just (EInt 0))]
       mainBody = SScope (mainInitStmts ++ [whileStmt] ++ mainFreeStmts)
    in GFuncDef Nothing TInt "main" [] mainBody
 
+-- | Returns a list of statements corresponding to actor function calls based
+-- on a provided Schedule. Note that actor function call statements include
+-- the associated minimum runtime enviorment statements for interacting with
+-- standard in and out.
 scheduleActors :: TranslationContext -> [String] -> [Statement]
 scheduleActors context scheduleList = aux scheduleList []
   where
@@ -68,36 +79,47 @@ scheduleActors context scheduleList = aux scheduleList []
               Nothing -> error ("schedule - actor not found: " ++ id)
          in aux idTail (acc ++ actorStmts)
 
+-- | Translates a component of the buffer size list provided by the SDF
+-- scheduler into an io token statment, initialisation statement, and freeing
+-- statement which are used to update the `TranslationContext`. Note that io
+-- token statements are only created for buffers which relate to with system
+-- inputs and outputs.
 translateBuffer :: TranslationContext -> (String, Int) -> TranslationContext
 translateBuffer initialContext (id, bufferSize) =
   let init = SVarDef (TPointer (TIdent "buffer_nonblocking")) id (ECall "buffer_nonblocking_new" [EInt bufferSize])
       free = SExpr (ECall "buffer_nonblocking_free" [EVar id])
-   in if (elem id (inputs initialContext))
+   in if (elem id (systemInputs initialContext))
         then
           let io_token = SVarDecl TInt ("input_" ++ id)
-           in initialContext {io_temporary_tokens = io_token : io_temporary_tokens initialContext, init_buffers = init : init_buffers initialContext, free_buffers = free : free_buffers initialContext}
+           in initialContext {ioTokens = io_token : ioTokens initialContext, initBuffers = init : initBuffers initialContext, freeBuffers = free : freeBuffers initialContext}
         else
-          if (elem id (outputs initialContext))
+          if (elem id (systemOutputs initialContext))
             then
               let io_token = SVarDecl TInt ("output_" ++ id)
-               in initialContext {io_temporary_tokens = io_token : io_temporary_tokens initialContext, init_buffers = init : init_buffers initialContext, free_buffers = free : free_buffers initialContext}
+               in initialContext {ioTokens = io_token : ioTokens initialContext, initBuffers = init : initBuffers initialContext, freeBuffers = free : freeBuffers initialContext}
             else
-              initialContext {init_buffers = init : init_buffers initialContext, free_buffers = free : free_buffers initialContext}
+              initialContext {initBuffers = init : initBuffers initialContext, freeBuffers = free : freeBuffers initialContext}
 
+-- | Translates an `IRConstructor` into a set of statements which are used to
+-- update the `TranslationContext`.
 translateIRConstructor :: TranslationContext -> IRConstructor -> TranslationContext
 translateIRConstructor initialContext constructor = case constructor of
   IRDelay _ tokens (inputSignal, outputSignal) ->
+    -- `IRDelay` results in a list of statements adding initial delay tokens
+    -- into a signal buffer.
     let maybeBufferName = lookup outputSignal (delayBuffers initialContext)
      in case maybeBufferName of
-          Just bufferName -> let delayStmts = auxDelay initialContext bufferName tokens [] in initialContext {delays = delayStmts ++ delays initialContext}
+          Just bufferName -> let delayStmts = auxDelay initialContext bufferName tokens [] in initialContext {initDelay = delayStmts ++ initDelay initialContext}
           Nothing -> error ("translateIRConstructor - delay buffer not found for signals: " ++ inputSignal ++ ", " ++ outputSignal)
   IRActor actorId actorType functionId (inputSignals, outputSignals) ->
+    -- `IRActor` results in a statement calling an SDF actor function within
+    -- source rates, target rates, and a function as arguments.
     let sourceRates = map (getSourceRate initialContext) inputSignals
         targetRates = map (getTargetRate initialContext) outputSignals
         actorName = translateActorType actorType
         translatedInputSignals = map (translateSignalId initialContext) inputSignals
         translatedOutputSignals = map (translateSignalId initialContext) outputSignals
-        s =
+        actorCallStmt =
           SExpr
             ( ECall
                 actorName
@@ -110,48 +132,68 @@ translateIRConstructor initialContext constructor = case constructor of
             )
         inputStmts = auxActor initialContext inputSignals []
         outputStmts = auxActor initialContext outputSignals []
-        stmts = inputStmts ++ [s] ++ outputStmts
+        stmts = inputStmts ++ [actorCallStmt] ++ outputStmts
         context1 = initialContext {actors = (actorId, stmts) : actors initialContext}
      in context1
   where
+    -- Helper function used to update a signal id into its respecitve signal
+    -- buffer id. There is only a difference in the translation for signals
+    -- which are source and targets of delays.
     translateSignalId :: TranslationContext -> String -> String
     translateSignalId context signalId =
       case lookup signalId (delayBuffers context) of
         Just bufferName -> bufferName
         Nothing -> signalId
-    auxActor :: TranslationContext -> [String] -> [Statement] -> [Statement]
-    auxActor context signals acc = case signals of
-      [] -> reverse acc
-      id : idsTail ->
-        if (elem id (inputs context))
-          then
-            let bufferSize = getSourceRate context id
-                scopeStmt =
-                  SScope
-                    [ SExpr (ECall "scanf" [EString "%d", EReference (EVar ("input_" ++ id))]),
-                      SExpr (ECall "write_token" [EVar id, EVar ("input_" ++ id)])
-                    ]
-                forStmt = SFor (SVarDef TInt "i" (EInt 0)) (EBinOp Less (EVar "i") (EInt (bufferSize))) (SExpr (EUnOp Increment (EVar "i"))) scopeStmt
-             in auxActor context idsTail (forStmt : acc)
-          else
-            if (elem id (outputs context))
-              then
-                let bufferSize = getTargetRate context id
-                    scopeStmt =
-                      SScope
-                        [ SExpr (ECall "read_token" [EVar id, EReference (EVar ("output_" ++ id))]),
-                          SExpr (ECall "printf" [EString "%d", EVar ("output_" ++ id)])
-                        ]
-                    forStmt = SFor (SVarDef TInt "i" (EInt 0)) (EBinOp Less (EVar "i") (EInt (bufferSize))) (SExpr (EUnOp Increment (EVar "i"))) scopeStmt
-                 in auxActor context idsTail ((SExpr (ECall "printf" [EString "\n"])) : forStmt : acc)
-              else
-                auxActor context idsTail acc
     auxDelay :: TranslationContext -> String -> [Int] -> [Statement] -> [Statement]
     auxDelay context id tokens acc = case tokens of
       [] -> reverse acc
       token : tokensTail ->
         let stmt = SExpr (ECall "write_token" [EVar id, EInt token])
          in auxDelay context id tokensTail (stmt : acc)
+    auxActor :: TranslationContext -> [String] -> [Statement] -> [Statement]
+    auxActor context signalList acc = case signalList of
+      [] -> reverse acc
+      id : idsTail ->
+        if (elem id (systemInputs context))
+          then
+            let bufferSize = getSourceRate context id
+                -- If actor has inputs which are system inputs the following
+                -- adds statements which relate to obtaining inputs from
+                -- standard in.
+                scopeStmt =
+                  SScope
+                    [ SExpr (ECall "scanf" [EString "%d", EReference (EVar ("input_" ++ id))]),
+                      SExpr (ECall "write_token" [EVar id, EVar ("input_" ++ id)])
+                    ]
+                forStmt =
+                  SFor
+                    (SVarDef TInt "i" (EInt 0))
+                    (EBinOp Less (EVar "i") (EInt (bufferSize)))
+                    (SExpr (EUnOp Increment (EVar "i")))
+                    scopeStmt
+             in auxActor context idsTail (forStmt : acc)
+          else
+            if (elem id (systemOutputs context))
+              then
+                let bufferSize = getTargetRate context id
+                    -- If actor has outputs which are system outputs the
+                    -- following adds statements which relate to printing
+                    -- outputs to standard out.
+                    scopeStmt =
+                      SScope
+                        [ SExpr (ECall "read_token" [EVar id, EReference (EVar ("output_" ++ id))]),
+                          SExpr (ECall "printf" [EString "%d", EVar ("output_" ++ id)])
+                        ]
+                    forStmt =
+                      SFor
+                        (SVarDef TInt "i" (EInt 0))
+                        (EBinOp Less (EVar "i") (EInt (bufferSize)))
+                        (SExpr (EUnOp Increment (EVar "i")))
+                        scopeStmt
+                    newLineStmt = SExpr (ECall "printf" [EString "\n"])
+                 in auxActor context idsTail (newLineStmt : forStmt : acc)
+              else
+                auxActor context idsTail acc
 
 translateActorType :: ActorType -> String
 translateActorType actorType = case actorType of
@@ -174,20 +216,20 @@ translateActorType actorType = case actorType of
 
 getSourceRate :: TranslationContext -> String -> Int
 getSourceRate context id =
-  let signal = lookup id (ir_signals context)
+  let signal = lookup id (lookupSignals context)
    in case signal of
         Just (IRSignal _ (_, rate) _) -> rate
         Nothing -> error ("getSourceRate - signal not found: " ++ id)
 
 getTargetRate :: TranslationContext -> String -> Int
 getTargetRate context id =
-  let signal = lookup id (ir_signals context)
+  let signal = lookup id (lookupSignals context)
    in case signal of
         Just (IRSignal _ _ (_, rate)) -> rate
         Nothing -> error ("getTargetRate - signal not found: " ++ id)
 
 getSignalById :: String -> [IRSignal] -> IRSignal
-getSignalById signalId signals = case signals of
+getSignalById signalId signalList = case signalList of
   [] -> error ("getSignalById - signal not found: " ++ signalId)
   (s@(IRSignal id _ _)) : tailSignals ->
     if id == signalId
@@ -199,6 +241,8 @@ translateIRFunctionToGlobals context (IRFunction id maybeFunction) = case maybeF
   Just function -> translateCoreExprToGlobals context id function
   Nothing -> []
 
+-- The following is a temporary hard coded solution that translates the inputs
+-- of the `add` and `accummulate` functions from SDF example 8.
 translateCoreExprToGlobals :: TranslationContext -> String -> CoreExpr -> [Global]
 translateCoreExprToGlobals context binder expr = case (binder, expr) of
   ("accumulate", Lam _ (Lam _ e)) ->
@@ -213,6 +257,9 @@ translateCoreExprToGlobals context binder expr = case (binder, expr) of
      in [g1, g2]
   _ -> error ("translateCoreExprToGlobals - unsupported expression:\n" ++ showPpr (flags context) expr)
 
+-- The following is a temporary hard coded solution that translates the
+-- contents and outputs of the `add` and `accumulate` functions from SDF
+-- example 8.
 translateCoreExprToStatement :: TranslationContext -> CoreExpr -> Statement
 translateCoreExprToStatement context expr = case expr of
   Var id -> SExpr (EVar (showPpr (flags context) id))
@@ -229,7 +276,7 @@ translateCoreExprToStatement context expr = case expr of
   Lam _ e -> translateCoreExprToStatement context e
   App _ e -> translateCoreExprToStatement context e
   Tick _ e -> translateCoreExprToStatement context e
-  _ -> error ("translateCoreExprToStatement - unsupported expression\n" ++ showPpr (flags context) expr)
+  _ -> error ("translateCoreExprToStatement - unsupported expression:\n" ++ showPpr (flags context) expr)
 
 translateAltsToStatements :: TranslationContext -> [Alt CoreBndr] -> Statement
 translateAltsToStatements context alts = case alts of
