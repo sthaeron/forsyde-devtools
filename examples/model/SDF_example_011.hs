5ad4c9f9ebdf94b2b72088c5c7ff3380c889bcdf
sthaeron <sebastian.thaeron@gmail.com>
2025-12-02 17:27:23 +0100

    feat: adds simple SDF examples to start Core IR to Procedural IR translation

---
+++ examples/model/SDF_example_011.hs
@@ -0,0 +1,17 @@
+module SDF_example_011 where
+
+import ForSyDe.Shallow
+
+-- Net list
+system :: Signal Int -> Signal Int
+system s_in = s_out
+  where
+    s_out = a_a s_in
+
+-- Process specifications
+a_a :: Signal Int -> Signal Int
+a_a s = actor11SDF 1 1 add s
+
+-- Function definitions
+add :: [Int] -> [Int]
+add [x] = [x + 1]
---
+++ examples/model/SDF_example_013.hs
@@ -0,0 +1,17 @@
+module SDF_example_013 where
+
+import ForSyDe.Shallow
+
+-- Net list
+system :: Signal Int -> Signal Int
+system s_in = s_out
+  where
+    s_out = a_a s_in
+
+-- Process specifications
+a_a :: Signal Int -> Signal Int
+a_a s = actor11SDF 1 2 add s
+
+-- Function definitions
+add :: [Int] -> [Int]
+add [x] = [x + 1, x + 2]
